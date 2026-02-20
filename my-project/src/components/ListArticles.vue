d3dd416921f7cca39ebf302e7f1b2f5b54be16a0
BitterSugar1 <gsaharok@yandex.ru>
2026-02-20 19:10:04 +0700

    15 block

--- my-project/src/components/ListArticles.vue
+++ my-project/src/components/ListArticles.vue
@@ -1,31 +1,95 @@
 <template>
   <main class="list-articles">
     <div class="list-articles__container">
+      <SearchForm class="list-articles__search-form" @search="onSearch" />
+
       <div class="list-articles__list">
         <CardArticle
           v-for="item in newsList"
           :key="item.id"
+          :id="item.id"
           :title="item.title"
-          :description="item.description"
+          :description="item.body"
           class="list-articles__card"
         />
       </div>
+        <ThePagniator
+          :page="page"
+          :totalPages="totalPages"
+          class="list-articles__paginator"
+          @setPage="setPage"
+        />
     </div>
   </main>
 </template>
 
+<CardArticle
+  v-for="item in newsList"
+  :key="item.id"
+  :id="item.id"
+  :title="item.title"
+  :description="item.description"
+  class="list-articles__card"
+/>
+
 <script>
   import CardArticle from "@/components/card/CardArticle.vue";
-  import newsList from "@/json/news.json";
+  import SearchForm from "@/components/SearchForm.vue";
 
   export default {
     components: {
       CardArticle,
+      SearchForm,
     },
     data() {
       return {
-        newsList: newsList,
+        newsList: [],
+        limit: 10,
+        page: 1,
+        totalCount: 1,
+        totalPages: 1,
+        search: "",
       };
+    },
+    methods: {
+      getNewsList(page = 1) {
+        const params = {
+          limit: this.limit,
+          skip: this.limit * (page - 1),
+        };
+
+        if (this.search) params.q = this.search;
+
+        const url = this.search
+          ? "https://dummyjson.com/posts/search"
+          : "https://dummyjson.com/posts";
+
+        this.$axios(url, {
+          params: params,
+        }).then((response) => {
+          if (response?.data?.posts) {
+            this.newsList = response.data.posts;
+            this.page = page;
+            this.totalCount = response.data.total;
+            this.totalPages = Math.ceil(this.totalCount / this.limit);
+          }
+        });
+      },
+      setPage(page) {
+        this.getNewsList(page);
+      },
+    onSearch(search) {
+        this.$router
+          .push({ query: search ? { search: search } : {} })
+          .then(() => {
+            this.search = this.$route.query.search;
+            this.getNewsList();
+          });
+      },
+    },
+    created() {
+      if (this.$route.query.search) this.search = this.$route.query.search;
+      this.getNewsList();
     },
   };
 </script>
@@ -38,5 +102,8 @@
     &__card {
       margin-bottom: 15px;
     }
+    &__search-form {
+    margin-bottom: 30px;
+  }
   }
 </style>
